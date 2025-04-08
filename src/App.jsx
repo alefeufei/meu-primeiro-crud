7238049888099f5a07c90bcc48c6c0c9cd868661
Alexandre Cruv <alefeufei@gmail.com>
2025-04-08 01:35:44 -0300

    rotast
    configurando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
-import { Feed } from "./pages/Feed";
+import { Router } from "./Router";
 
 function App() {
   return (
     <>
       <Header />
-      <Feed />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
