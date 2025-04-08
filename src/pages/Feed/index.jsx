f3c9767fba7dcbe9240ba73245754263ab99fc57
Alexandre Cruv <alefeufei@gmail.com>
2025-04-08 16:01:29 -0300

    UseEffect
    preparado o useffect e catch erro

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../../components/Card'
 import './styles.css'
+import { api } from '../../lib/axios';
 
 export function Feed() {
+
+const [posts, setPosts] = useState([])
+
+useEffect(() => {
+  api.get('/posts')
+  .then((response) => {
+    setPosts(response.data)
+  })
+  .catch((error) => {
+    console.error('Error fetching posts:', error);
+  });
+
+}, [])
+
   return (
     <div className="feedContainer">
-      <Card />
+   <Card/>
     </div>
   );
 }
